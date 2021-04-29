ea01733c800758269622751aa5c2a765689c319d
yaelahsas <dhimaspanji66@gmail.com>
2021-04-30 02:11:28 +0700

    update custom

--- app/src/test/java/com/karina/tracking/ExampleUnitTest.kt
+++ app/src/test/java/com/karina/tracking/ExampleUnitTest.kt
@@ -1,3 +1,12 @@
+/*
+ * *
+ *  * Created by Dhimas Panji Sastra on 4/30/21 2:11 AM
+ *  * Copyright (c) $year . All rights reserved.
+ *  * Last modified 4/30/21 2:10 AM
+ *  * Made With ❤ for U
+ *
+ */
+
 package com.karina.tracking
 
 import org.junit.Assert.assertEquals
